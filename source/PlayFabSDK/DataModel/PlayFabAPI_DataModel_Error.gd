ebfb47689068a62614ba635a33425bd172313f5b
airreader <airreader@gmail.com>
2023-04-20 09:00:36 +0900

    is_empty

--- source/PlayFabSDK/DataModel/PlayFabAPI_DataModel_Error.gd
+++ source/PlayFabSDK/DataModel/PlayFabAPI_DataModel_Error.gd
@@ -10,25 +10,25 @@ class PFApiErrorWrapper:
     var errorMessage: String
     var errorDetail: Dictionary
 
-    func _init(dict_param: Dictionary = {}):
+    func _init(_dict_param: Dictionary = {}):
         
         self.errorDetail = {}
-        self.set_dict(dict_param);
+        self.set_dict(_dict_param);
 
-    func set_dict(dict_param: Dictionary):
+    func set_dict(_dict_param: Dictionary):
         
-        if "code" in dict_param:
-            self.code = dict_param["code"]
-        if "status" in dict_param:
-            self.status = dict_param["status"]
-        if "error" in dict_param:
-            self.error = dict_param["error"]
-        if "errorCode" in dict_param:
-            self.errorCode = dict_param["errorCode"]
-        if "errorMessage" in dict_param:
-            self.errorMessage = dict_param["errorMessage"]
-        if "errorDetail" in dict_param:
-            self.errorDetail = dict_param["errorDetail"]
+        if "code" in _dict_param:
+            self.code = _dict_param["code"]
+        if "status" in _dict_param:
+            self.status = _dict_param["status"]
+        if "error" in _dict_param:
+            self.error = _dict_param["error"]
+        if "errorCode" in _dict_param:
+            self.errorCode = _dict_param["errorCode"]
+        if "errorMessage" in _dict_param:
+            self.errorMessage = _dict_param["errorMessage"]
+        if "errorDetail" in _dict_param:
+            self.errorDetail = _dict_param["errorDetail"]
 
     func get_dict() -> Dictionary:
         
@@ -37,15 +37,15 @@ class PFApiErrorWrapper:
         if self.code != null:
             dict_result["code"] = self.code
         if self.status != null:
-            if self.status.empty() != true:
+            if self.status.is_empty() != true:
                 dict_result["status"] = self.status
         if self.error != null:
-            if self.error.empty() != true:
+            if self.error.is_empty() != true:
                 dict_result["error"] = self.error
         if self.errorCode != null:
             dict_result["errorCode"] = self.errorCode
         if self.errorMessage != null:
-            if self.errorMessage.empty() != true:
+            if self.errorMessage.is_empty() != true:
                 dict_result["errorMessage"] = self.errorMessage
         if self.errorDetail != null:
             dict_result["errorDetail"] = self.errorDetail
